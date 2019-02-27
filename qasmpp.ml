6d479d3e81fc729122a1c141c982c302a794e3b7
Chet Murthy <chetsky@gmail.com>
2019-02-27 14:12:19 -0800

    pretty-printer for ASTs; but no tests

--- qasmpp.ml
+++ qasmpp.ml
@@ -114,3 +114,125 @@ let pr_comma = (fun () -> [< '", " >])
     String.concat "" l
 
 end
+
+module ASTPP = struct
+
+  let header vers = [< 'Printf.sprintf "OPENQASM %s;\n" vers >]
+
+  let expr pr_id e =
+    let rec expr0 = function
+      | AST.ID id -> [< pr_id id >]
+      | AST.NNINT n -> [< 'string_of_int n >]
+      | AST.REAL r -> [< 'r >]
+      | AST.PI -> [< '"pi" >]
+      | AST.SIN e -> [< '"sin(" ; expr e ; '")" >]
+      | AST.COS e -> [< '"cos(" ; expr e ; '")" >]
+      | AST.TAN e -> [< '"tan(" ; expr e ; '")" >]
+      | AST.EXP e -> [< '"exp(" ; expr e ; '")" >]
+      | AST.LN e -> [< '"ln(" ; expr e ; '")" >]
+      | AST.SQRT e -> [< '"sqrt(" ; expr e ; '")" >]
+      | e -> [< '"(" ; expr e ; '")" >]
+
+    and expr1 = function
+      | AST.XOR(e1, e2) -> [< expr0 e1 ; '" ^ " ; expr1 e2 >]
+      | e -> expr0 e
+
+    and expr2 = function
+      | AST.UMINUS e -> [< '"-" ; expr2 e >]
+      | e -> expr1 e
+
+    and expr3 = function
+      | AST.MUL(e1, e2) -> [< expr3 e1 ; '" * " ; expr2 e2 >]
+      | AST.DIV(e1, e2) -> [< expr3 e1 ; '" / " ; expr2 e2 >]
+      | e -> expr2 e
+
+    and expr4 = function
+      | AST.ADD(e1, e2) -> [< expr4 e1 ; '" + " ; expr3 e2 >]
+      | AST.SUB(e1, e2) -> [< expr4 e1 ; '" - " ; expr3 e2 >]
+      | e -> expr3 e
+
+    and expr e = expr4 e
+  in expr e
+
+  let or_indexed pr_it = function
+    | AST.INDEXED (id, n) -> [< pr_it id ; '"[" ; 'string_of_int n ; '"]" >]
+    | AST.IT id -> pr_it id
+
+  let pr_comma = (fun () -> [< '", " >])
+
+  let pr_qreg (AST.QREG id) = [< 'id >]
+  let pr_creg (AST.CREG id) = [< 'id >]
+
+  let raw_uop ~pr_cparamvar ~pr_qregvar ?(prefix="") = function
+    | AST.U(el, a) -> [< 'prefix ; '"U(" ; prlist_with_sep pr_comma (expr pr_cparamvar) el ; '") " ; pr_qregvar a ; '";\n" >]
+    | AST.CX(l, r) -> [< 'prefix ; '"CX " ; pr_qregvar l ; '", "; pr_qregvar r ; '";\n"; >]
+    | AST.COMPOSITE_GATE(gateid, params, regs) ->
+       [< 'prefix ; 'gateid ; '" (" ; prlist_with_sep pr_comma (expr pr_cparamvar) params  ; '") " ; prlist_with_sep pr_comma pr_qregvar regs ; '";\n" >]
+
+  let pr_empty _ = assert false
+  let raw_qop ?(prefix="") = function
+    | AST.UOP u -> raw_uop ~pr_cparamvar:pr_empty ~pr_qregvar:(or_indexed pr_qreg) ~prefix u
+    | AST.MEASURE(l, r) -> [< 'prefix ; '"measure " ; or_indexed pr_qreg l ; '", "; or_indexed pr_creg r ; '";\n" >]
+    | AST.RESET l -> [< 'prefix ; '"reset " ; or_indexed pr_qreg l ; '";\n" >]
+
+  let qop ~prefix (aux, i) =
+    let commentstring = TA.comment_string aux in
+    match commentstring with
+    | "" -> raw_qop ~prefix i
+    | _ -> [< 'commentstring ; raw_qop ~prefix i >]
+
+  let pr_cparamvar (AST.CPARAMVAR s) = [< 's >]
+
+  let pr_qubit (AST.QUBIT id) = [< 'id >]
+
+  let raw_gate_op = function
+    | AST.GATE_UOP i -> raw_uop ~pr_cparamvar ~pr_qregvar:pr_qubit ~prefix:"  " i
+    | AST.GATE_BARRIER l -> [< '"  " ; '"barrier "; prlist_with_sep pr_comma pr_qubit l ; '";\n" >]
+
+  let gate_op (aux, gop) =
+    let commentstring = TA.comment_string aux in
+    match commentstring with
+    | "" -> raw_gate_op gop
+    | _ -> [< 'commentstring ; raw_gate_op gop >]
+
+  let pr_id id = [< 'id >]
+
+  let raw_stmt = function
+    | AST.STMT_GATEDECL(gateid, formal_params, formal_bits, gopl) ->
+       [< '"gate " ; 'gateid ; '" (" ; prlist_with_sep pr_comma pr_id formal_params ; '") " ;
+        prlist_with_sep pr_comma pr_id formal_bits ; '" {\n" ;
+        prlist gate_op gopl ;
+        '"}\n" >]
+    | AST.STMT_OPAQUEDECL(gateid, formal_params, formal_bits) ->
+       [< '"opaque " ; 'gateid ; '" (" ; prlist_with_sep pr_comma pr_id formal_params ; '") " ;
+        prlist_with_sep pr_comma pr_id formal_bits ; '";\n" >]
+    | AST.STMT_QOP i -> raw_qop i
+    | AST.STMT_IF(reg, n, i) ->
+       [< '"if (" ; pr_creg reg ; '"=="; 'string_of_int n ; '") "; raw_qop i >]
+    | AST.STMT_BARRIER l ->
+       [< '"barrier " ; prlist_with_sep pr_comma (or_indexed pr_qreg) l ; '";\n" >]
+    | AST.STMT_QREG(id, n) ->
+       [< '"qreg " ; 'id ; '"[" ; 'string_of_int n ; '"];\n" >]
+    | AST.STMT_CREG(id, n) ->
+       [< '"creg " ; 'id ; '"[" ; 'string_of_int n ; '"];\n" >]
+
+  let stmt (aux, s) =
+    let commentstring = TA.comment_string aux in
+    match commentstring with
+    | "" -> raw_stmt s
+    | _ -> [< 'commentstring ; raw_stmt s >]
+
+  let program l =
+    prlist stmt l
+
+  let main (vers, l) =
+    [< '"OPENQASM " ; 'vers ; '";\n" ;
+     program l >]
+
+  let pp ppfun arg =
+    let strm = ppfun arg in
+    let l = list_of_stream strm in
+    String.concat "" l
+
+end
+                 
