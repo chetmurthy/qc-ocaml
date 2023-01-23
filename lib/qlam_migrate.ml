84c369905b38ee25010644d0f589d00e6ce0e2f6
Chet Murthy <chetsky@gmail.com>
2023-01-23 15:29:21 -0800

    environment variables in ya-wrap-camlfind

--- lib/qlam_migrate.ml
+++ lib/qlam_migrate.ml
@@ -1,4 +1,4 @@
-(** -syntax camlp5o -package pa_ppx.import,pa_ppx_migrate -ppopt -pa_import-I -ppopt . *)
+(** -syntax camlp5o -package $(MIGRATE_PACKAGES) $(MIGRATE_CFLAGS) *)
 exception Migration_error of string
 
 let migration_error feature =
